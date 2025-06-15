499134c224136455e8345f25e0da00208c95d64c
aashisoni000 <aashi2701soni@gmail.com>
2025-06-16 00:17:28 +0530

    done for the day

--- firstjs.js
+++ firstjs.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+let fullName = "tony";
+fullName = "aashi";
+console.log(fullName);
+const product = {
+    title:"ball pen",
+    rating:4.5,
+    offer:5,
+    price: 270,
+};
+console.log(product);
+
+const profile = {
+    username: "aashi",
+    followers: 2333,
+
+};
+
+console.log(profile);
