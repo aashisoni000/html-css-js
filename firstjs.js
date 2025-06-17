e697dcf61a1dea275548287fc0c2052174f5c8e9
aashisoni000 <aashi2701soni@gmail.com>
2025-06-17 23:57:50 +0530

    okay one more

--- firstjs.js
+++ firstjs.js
@@ -2,14 +2,6 @@ console.log("Hello World");
 let a=6;
 let b=10;
 console.log("a is equal to", a, "and b is equal to", b);
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a**b);
-console.log(a/b);
-console.log(a%b);
-console.log(a++);
-console.log(a);
-console.log(b++);
-console.log(b);
+console.log(a==b);
+let c="10"
 
